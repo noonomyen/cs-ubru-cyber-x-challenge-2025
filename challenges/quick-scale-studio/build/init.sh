e4dd0cea7e57d76bd67936bdae2d83e2cba10b37
c0ffeeOverdose <std.66122420117@ubru.ac.th>
2025-09-21 21:21:28 +0700

    optimize docker image size chall 'quick-scale-studio'

--- challenges/quick-scale-studio/build/init.sh
+++ challenges/quick-scale-studio/build/init.sh
@@ -12,4 +12,22 @@ unset GZCTF_FLAG
 mkdir -p /var/www/html/uploads /var/www/html/processed
 chown -R www-data:www-data /var/www/html/uploads /var/www/html/processed || true
 
-exec apache2-foreground
+: "${APACHE_CONFDIR:=/etc/apache2}"
+: "${APACHE_ENVVARS:=$APACHE_CONFDIR/envvars}"
+if [[ -f "$APACHE_ENVVARS" ]]; then
+    # shellcheck disable=SC1090
+    . "$APACHE_ENVVARS"
+fi
+
+: "${APACHE_RUN_DIR:=/var/run/apache2}"
+: "${APACHE_PID_FILE:=$APACHE_RUN_DIR/apache2.pid}"
+rm -f "$APACHE_PID_FILE"
+
+for env_var in $(env | cut -d= -f1 | grep '^APACHE_.*_DIR$'); do
+    dir_path="${!env_var}"
+    if [[ "$dir_path" == /* ]]; then
+        mkdir -p "$dir_path"
+    fi
+done
+
+exec /usr/sbin/apache2 -DFOREGROUND
