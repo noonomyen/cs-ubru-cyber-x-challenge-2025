62d6251b9d00323d1d77c3e26c6213518b1fea4e
noonomyen <nomyen@noonomyen.com>
2025-09-25 03:50:08 +0700

    add chall 'ask-real'

---
+++ challenges/ask-real/src/gen.py
@@ -0,0 +1,8 @@
+from base64 import b64encode
+
+flag = b"CSUBRU{7h47_w45_fun_bu7_1_wund3r_h0w_l0ng_17_w0u1d_74k3_b3f0r3_f1nd1ng_17}"
+
+for i in range(20):
+    flag = b64encode(flag)
+
+open("ask-real.txt", "w").write(flag.decode())
